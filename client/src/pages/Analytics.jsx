0ee62fc6bf40a9b60b808bf2da21d8db27f6f0f3
Junx27 <junxitsumo@gmail.com>
2023-07-01 11:56:14 +0700

    fix header

---
+++ client/src/pages/Analytics.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Analytics() {
+  return <div>this Analytic Pages</div>;
+}
+
+export default Analytics;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <div style={{ height: "1000px" }}>cek</div>
+    </div>
+  );
+}
+
+export default Home;
